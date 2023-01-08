44433ae0d18b643358148fdf8e2c46f519c3edd4
kesarsauce <50715271+kesarsauce@users.noreply.github.com>
2023-01-08 23:24:07 +0530

    test case #3

--- scraper.py
+++ scraper.py
@@ -90,7 +90,7 @@ def trigger_match_thread():
 
     # sort ascending order by date
     match_pipeline = [dict(match_id=k, match_date=matches_data[k]) for k in sorted(matches_data, key=matches_data.get)]
-    match_pipeline = [dict(match_id="54321", match_date=datetime(year=2023, month=1, day=8, hour=18, minute=50))]
+    match_pipeline = [dict(match_id="543210", match_date=datetime(year=2023, month=1, day=8, hour=17, minute=0))]
 
     app.logger.info(f"Matches in pipeline: {len(match_pipeline)}")
 
